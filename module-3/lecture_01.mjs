c8ad91f0d2326ca0fd8dd5cac3c6788c0d8993a7
Chanetteberg <chanetteb@uia.no>
2025-09-20 13:13:40 +0200

    Ferdig med task 3_1 :)

--- module-3/lecture_01.mjs
+++ module-3/lecture_01.mjs
@@ -1,2 +1,40 @@
 "use strict"
 import {printOut} from "../../common/script/utils.mjs";
+
+const text1 = "Du er gammel nok til å ta ed en som ikke er gammel nok.";
+const text2 = "Du kan gå på kino!";
+const text3 = "Beklager, du er ikke gammel nok";
+
+const ageMovie = 16;
+const ageBringAlong = 18;
+let age1 = 15; // Gammel nok til å ha med seg en person under 16.
+let age2 = 19; // Personen er ikke gammel nok til å gå alene.
+
+if(age1 < age2) {
+    const age1Old = age1;
+    age1 = age2;
+    age2 = age1Old;
+ }
+
+
+
+
+if (age1 >= ageMovie) {
+    printOut("person 1: " + text2);
+    if (age2 >= ageMovie) {
+        printOut("person 2: " + text2);
+    } else if (age1 >= ageBringAlong) {
+        printOut("person 1: " + text1);
+        printOut("person 2: " + text2);
+    } else {
+        printOut("person 2: " + text3);
+    }
+}else {
+    printOut("vi har ingen case for dette!");
+
+
+
+
+
+
+
--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -4,39 +4,117 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+let wakeUpTime = 8;
+if ( wakeUpTime == 7) {
+  printOut("if i wake up exactly 7´o clock then i catch the bus to school. ");
+} else if (wakeUpTime == 8 ) {
+   {printOut("if i wake up later than 8´o clock ,then i can take the train  to school. "); }
+} else {
+  printOut("Otherwise i have to take the car to school.");
 }
-printOut(newLine);
+
+printOut(newLine)
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("check if a number is positive, negative or zero");
+
+let number = 0;
+
+if (number > 0) {
+  printOut ("Positive");
+} else if (number < 0) {
+  printOut ("Negative");
+} else {
+  printOut ("Zero");
+}
+
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+let imageSizeMP = Math.floor(Math.random() * 8) + 1;
+printOut ("Uploaded image size : " + imageSizeMP + "MP" );
+
+if (imageSizeMP >= 4 ) {
+  printOut ("Thank you");
+} else {
+  printOut ("The Image too small");
+
+} 
+  printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let imageSizeMP2 = Math.floor(Math.random()* 8) +1;
+printOut ("Uploaded image size : " + imageSizeMP2 + "MP");
+
+if (imageSizeMP >= 6 ) {
+  printOut ("Image is too large");
+} else if (imageSizeMP2 >= 4 ) {
+  printOut ("Thank you");
+} else {
+  printOut ("The Image too small");
+}
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const monthList = ["january", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random()* noOfMonth)];
+printOut ("Random month: " + monthName);
+
+if (monthName.toLowerCase(). includes ("r")) {
+  printOut(" You must take vitamin D");
+} else {
+  printOut(" You don't need to take vitamin D");
+}
 printOut(newLine);
+
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const monthList2= ["january", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const noOfMonth2 = monthList2.length;
+const monthName2 = monthList2[Math.floor(Math.random()* noOfMonth2)];
+printOut ("Random month: " + monthName2);
+
+if ("November", "December", "January", "February", "March".includes(monthName2)) {
+  printOut(" You must take vitamin D");
+  } else {
+  printOut(" You don't need to take vitamin D");
+  }
+
+let daysInMonth2; 
+if (["April", "June", "September", "November"].includes(monthName2)) {
+daysInMonth2 = 30;
+} else if (monthName2 === "February") {
+  daysInMonth2 = 28;
+} else {
+  daysInMonth2 = 31;
+}
+printOut("Number of days in " + monthName2 + ": " + daysInMonth2);
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList3= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const noOfMonth3 = monthList3.length;
+const monthName3 = monthList3[Math.floor(Math.random()* noOfMonth3)];
+printOut ("Random month: " + monthName3);
+
+if (monthName3 === "March" || monthName3 === "May") {
+  printOut ("The gallery is closed for refurbishment.");
+} else if (monthName3 === "April") {
+  printOut ("The gallery is open in temporary premises next door.");
+}else {
+  printOut ("The gallery is open, welcome!");
+} 
+
+
 printOut(newLine);
